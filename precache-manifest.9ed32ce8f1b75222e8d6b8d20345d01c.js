1e7e72b420b022f88388b4f4b42ddf724e07268e
avatar92 <adelbensalem1992@gmail.com>
2019-06-18 00:22:56 +0200

    Updates

---
+++ precache-manifest.9ed32ce8f1b75222e8d6b8d20345d01c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b37cc8f4d8c1a21c15905ac4d302215c",
+    "url": "/dynamic-credit-card/index.html"
+  },
+  {
+    "revision": "d6c6462dc693d6f9db23",
+    "url": "/dynamic-credit-card/static/css/main.dc458030.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/dynamic-credit-card/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "d6c6462dc693d6f9db23",
+    "url": "/dynamic-credit-card/static/js/main.08ca1241.chunk.js"
+  },
+  {
+    "revision": "0a3602f357a6ae2c1913",
+    "url": "/dynamic-credit-card/static/js/runtime~main.a8842e53.js"
+  },
+  {
+    "revision": "57c4f2c371733d649dfa2ce1bf0c0f8c",
+    "url": "/dynamic-credit-card/static/media/creditcardbg.57c4f2c3.jpg"
+  }
+]);
